e2e5037b83deea539bd82c8c4ac3ee2d9353c2fb
O.Kuloverova <oliich23@mail.ru>
2026-03-05 21:19:21 +0400

    add search and burger

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,7 +1,10 @@
 function header() {
+  const header = document.querySelector('.header');
+
+  if (!header) return;
   function headerDesk() {
-    const allModals = document.querySelectorAll('[data-modal]');
-    const allTriggers = document.querySelectorAll('[data-trigger]');
+    const allModals = header.querySelectorAll('[data-modal]');
+    const allTriggers = header.querySelectorAll('[data-trigger]');
 
     if (allModals.length === 0 || allTriggers.length === 0) return;
 
@@ -9,6 +12,7 @@ function header() {
 
     toggleModals();
     initTabs();
+    initSearch();
 
     function closeEverything() {
       allModals.forEach((m) => m.classList.remove('isOpen'));
@@ -83,8 +87,116 @@ function header() {
     }
   }
 
-  function headerMob() {}
+  function headerMob() {
+    const burger = header.querySelector('.burger');
+    const burgerModal = header.querySelector('.burger-modal');
+    const asides = header.querySelectorAll('.burger-modal__aside');
+    burger.addEventListener('click', () => {
+      const isOpening = !header.classList.contains('isBurger');
+      header.classList.toggle('isBurger', isOpening);
 
+      if (window.innerWidth <= 768 && isOpening) {
+        document.body.classList.add('no-scroll');
+        document.documentElement.classList.add('lock');
+
+        scrollToTop();
+      }
+
+      isOpening && header.classList.remove('isSearch');
+      if (!isOpening) {
+        asides.forEach((item) => item.classList.remove('isOpen'));
+      }
+    });
+
+    burgerModal.addEventListener('click', (e) => {
+      const trigger = e.target.closest('[data-burger-trigger]');
+      const back = e.target.closest('[data-burger-back]');
+
+      if (trigger) {
+        const targetId = trigger.dataset.burgerTrigger;
+        const targetModal = document.querySelector(`[data-burger-aside="${targetId}"]`);
+
+        if (targetId) {
+          targetModal.classList.add('isOpen');
+        }
+        return;
+      }
+
+      if (back) {
+        back.closest('[data-burger-aside]').classList.remove('isOpen');
+      }
+    });
+  }
+
+  function initSearch() {
+    const search = header.querySelector('.search');
+    if (!search) return;
+    const searchInput = search.querySelector('input');
+
+    const searchClear = search.querySelector('.search__clear');
+    const searchModal = search.querySelector('.search__modal');
+
+    searchModal.classList.add('isTransition');
+    document.addEventListener('click', (e) => {
+      const isOpenBtn = e.target.closest('.search__open');
+      const isInsideSearch = e.target.closest('.search');
+      const closeBtn = e.target.closest('.search__close-btn');
+      if (isOpenBtn) {
+        header.classList.add('isSearch');
+        header.classList.remove('isBurger');
+
+        header.querySelectorAll('.burger-modal__aside').forEach((item) => item.classList.remove('isOpen'));
+
+        searchInput.focus();
+        if (window.innerWidth <= 768) {
+          document.body.classList.add('no-scroll');
+          document.documentElement.classList.add('lock');
+
+          scrollToTop();
+        }
+      }
+      if ((!isInsideSearch && window.innerWidth > 768) || closeBtn) {
+        header.classList.remove('isSearch');
+        document.body.classList.remove('no-scroll');
+        document.documentElement.classList.remove('lock');
+      }
+    });
+
+    searchInput.addEventListener('input', (e) => {
+      let value = e.target.value.trim().toLowerCase();
+
+      if (value.length > 0) {
+        search.classList.add('isFilled');
+      } else {
+        search.classList.remove('isFilled');
+      }
+    });
+
+    searchClear.addEventListener('click', () => {
+      searchInput.value = '';
+      searchInput.focus();
+      search.classList.remove('isFilled');
+    });
+  }
+
+  function scrollToTop(duration = 400) {
+    const start = window.pageYOffset;
+    const startTime = performance.now();
+
+    function animate(time) {
+      const elapsed = time - startTime;
+      const progress = Math.min(elapsed / duration, 1);
+
+      window.scrollTo(0, start * (1 - progress));
+
+      if (progress < 1) {
+        requestAnimationFrame(animate);
+      }
+    }
+    requestAnimationFrame(animate);
+  }
+
+  initSearch();
   window.innerWidth > 768 ? headerDesk() : headerMob();
 }
 
